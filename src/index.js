a3ab0cc6b06a77c56600d6e625770470a377f46e
mike <phlykes@gmail.com>
2023-12-01 17:04:19 -0500

    Git ignore index created Main

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './format.css'
+import './style.css'
+import {taskinfo} from './modules'
+
+console.log("imhere")
+let b1 =  document.querySelector("#addtask")
+
+b1.addEventListener('onclick', function (e){
+    
+
+})
---
+++ src/modules.js
@@ -0,0 +1,12 @@
+function taskinfo(a,b,c,d,e,f,g){
+    this.name = a
+    this.desc = b
+    this.date = c
+    this.due = d
+    this.priority = e
+    this.goals = f
+    this.completion =  g
+    console.log("sinjfjnf")
+    return this
+}
+export{taskinfo}
